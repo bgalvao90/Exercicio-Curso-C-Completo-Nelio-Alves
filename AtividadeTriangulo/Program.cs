0ba86b3e69991582b0bac00343f82e77168fa4b3
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-18 17:58:20 -0300

    AtividadesC#

---
+++ AtividadeTriangulo/Program.cs
@@ -0,0 +1,38 @@
+﻿using AtividadeTriangulo;
+using System.Globalization;
+
+
+
+Triangulo x = new Triangulo();
+Triangulo y = new Triangulo();
+
+Console.WriteLine("Entre com as medidas do triângulo X:");
+
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Entre com as medidas do triângulo Y:");
+
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double p = (x.A + x.B + x.C) / 2.0;
+double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+p = (y.A + y.B + y.C) / 2.0;
+double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+Console.WriteLine("Area do triangulo X : "+ areaX.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Area do triangulo Y : "+ areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+if(areaX > areaY)
+{
+    Console.WriteLine("Maior área: X");
+}
+else
+{
+    Console.WriteLine("Maior área: Y");
+}
